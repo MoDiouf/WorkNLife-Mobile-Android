95d54a15fc902661d754c41526233cee3232109c
MohamedDIOUF <mohameddiouf096@gmail.com>
2025-11-23 14:39:54 +0000

    okay

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,23 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
-module.exports = (async () => {
+module.exports = (() => {
   const config = getDefaultConfig(__dirname);
-  const { transformer, resolver } = config;
 
   config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer")
+    ...config.transformer,
+    babelTransformerPath: require.resolve("react-native-svg-transformer"),
   };
   config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter(ext => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"]
+    ...config.resolver,
+    assetExts: config.resolver.assetExts.filter(ext => ext !== "svg"),
+    sourceExts: [...config.resolver.sourceExts, "svg", "ts", "tsx"],
   };
+  config.transformer.getTransformOptions = async () => ({
+    transform: {
+      experimentalImportSupport: false,
+      inlineRequires: true,
+    },
+  });
 
   return config;
 })();
